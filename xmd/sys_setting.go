01ac95cd0b68085d9073d132b6cf66505ef17bd0
miaojingyi <miaojingyi86@gmail.com>
2023-02-12 02:55:19 +0800

    系统设置

--- xmd/sys_setting.go
+++ xmd/sys_setting.go
@@ -5,34 +5,30 @@ import (
 	"go-phoenix/asql"
 	"go-phoenix/base"
 	"go-phoenix/handle"
-	"strings"
 )
 
 type SysSetting struct {
 }
 
 func (m *SysSetting) Get(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
-	scope := ctx.FormValue("scope")
-
-	if strings.EqualFold(scope, "LOGIN") {
-		res, err := asql.Select(tx, "SELECT field_, value_ FROM sys_setting")
-		if err != nil {
-			return nil, err
-		}
-
-		newRes, _ := base.ResAsMapSlice(res, true, "field_", "value_")
-		return newRes, nil
-	}
-
-	query := `
-		SELECT id, code_, name_, description_, create_at_, update_at_
-		FROM wf_diagram
-		ORDER BY order_ ASC
-	`
-	res, err := asql.Select(tx, query)
+	res, err := asql.Select(tx, "SELECT field_, value_ FROM sys_setting")
 	if err != nil {
 		return nil, err
 	}
 
-	return res, nil
+	newRes, _ := base.ResAsMapSlice(res, true, "field_", "value_")
+	return newRes, nil
+}
+
+func (m *SysSetting) Post(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
+	values := ctx.GetValues()
+
+	for field, value := range values {
+		query := "UPDATE sys_setting SET value_ = ?, update_at_ = ? WHERE field_ = ?"
+		if err := asql.Update(tx, query, value, asql.GetNow(), field); err != nil {
+			return nil, err
+		}
+	}
+
+	return map[string]string{"status": "success"}, nil
 }
