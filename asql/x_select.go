76ba580738200997a10420e7d331901c2a1395b6
苗景译 <miaojingyi86@gmail.com>
2024-08-09 16:28:18 +0800

    .

--- asql/x_select.go
+++ asql/x_select.go
@@ -2,13 +2,29 @@ package asql
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"strings"
 	"time"
+	"unicode"
 )
 
 func Select(tx *sql.Tx, query string, args ...interface{}) ([]map[string]string, error) {
-	logrus.Debugf("%s %s", FnArgs(args...), query)
+	if logrus.IsLevelEnabled(logrus.DebugLevel) {
+		var prefix string
+
+		// 格式化SQL输出
+		for i := 5; i > 1; i-- {
+			prefix = fmt.Sprintf("\n%s", strings.Repeat("\t", i))
+			if strings.HasPrefix(query, prefix) {
+				break
+			}
+		}
+		query = strings.ReplaceAll(query, prefix, "\n\t")
+
+		query = strings.TrimRightFunc(query, unicode.IsSpace)
+		logrus.Debugf("%s %s", FnArgs(args...), query)
+	}
 
 	// Rows
 	rows, err := tx.Query(query, args...)
