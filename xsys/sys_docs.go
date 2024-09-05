5542b5635614f84335a12ecef37b27665ea615b2
苗景译 <miaojingyi86@gmail.com>
2024-09-06 00:46:13 +0800

    .

--- xsys/sys_docs.go
+++ xsys/sys_docs.go
@@ -34,8 +34,47 @@ func (o *SysDocs) Get(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
 	return asql.Select(tx, query, args...)
 }
 
+func (o *SysDocs) GetDownload(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
+	docId := ctx.FormValue("id")
+
+	var dir, mime string
+
+	if err := asql.SelectRow(tx, "SELECT dir_, mime_ FROM sys_doc WHERE id = ?", docId).Scan(&dir, &mime); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("无效的文档ID %q", docId)
+		}
+
+		return nil, err
+	}
+
+	// 获取文件
+	rFile, err := os.Open(filepath.Join(base.Config.Dir(), dir, docId))
+	if err != nil {
+		return nil, err
+	}
+	defer rFile.Close()
+
+	// 写入文件
+	ctx.Writer.Header().Set("Content-Type", mime)
+	if _, err := io.Copy(ctx.Writer, rFile); err != nil {
+		return nil, err
+	}
+
+	return map[string]string{"status": "success"}, nil
+}
+
+// PostImport 文档上传
+func (o *SysDocs) PostImport(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
+	return o.save(tx, ctx, "import")
+}
+
 // PostUpload 文档上传
 func (o *SysDocs) PostUpload(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
+	return o.save(tx, ctx, "upload")
+}
+
+// PostUpload 文档上传
+func (o *SysDocs) save(tx *sql.Tx, ctx *handle.Context, directory string) (interface{}, error) {
 	if err := ctx.ParseMultipartForm(1 << 30); err != nil {
 		return nil, err
 	}
@@ -62,7 +101,7 @@ func (o *SysDocs) PostUpload(tx *sql.Tx, ctx *handle.Context) (interface{}, erro
 
 	// 创建目录
 	p1, p2 := hex.EncodeToString(k4[:1]), hex.EncodeToString(k4[1:])
-	dir := filepath.Join("store", "upload", time.Now().Format("0601"), p1, p2)
+	dir := filepath.Join("store", directory, time.Now().Format("0601"), p1, p2)
 	if _, err := os.Stat(dir); err != nil {
 		if !os.IsNotExist(err) {
 			return nil, err
@@ -102,33 +141,4 @@ func (o *SysDocs) PostUpload(tx *sql.Tx, ctx *handle.Context) (interface{}, erro
 	}
 
 	return map[string]interface{}{"status": "server", "id": docId, "value": docId}, nil
-}
-
-func (o *SysDocs) PostDownload(tx *sql.Tx, ctx *handle.Context) (interface{}, error) {
-	docId := ctx.PostFormValue("id")
-
-	var dir, mime string
-
-	if err := asql.SelectRow(tx, "SELECT dir_, mime_ FROM sys_doc WHERE id = ?", docId).Scan(&dir, &mime); err != nil {
-		if err == sql.ErrNoRows {
-			return nil, fmt.Errorf("无效的文档ID %q", docId)
-		}
-
-		return nil, err
-	}
-
-	// 获取文件
-	rFile, err := os.Open(filepath.Join(base.Config.Dir(), dir, docId))
-	if err != nil {
-		return nil, err
-	}
-	defer rFile.Close()
-
-	// 写入文件
-	ctx.Writer.Header().Set("Content-Type", mime)
-	if _, err := io.Copy(ctx.Writer, rFile); err != nil {
-		return nil, err
-	}
-
-	return map[string]string{"status": "success"}, nil
 }
