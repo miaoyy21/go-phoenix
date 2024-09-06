02f405b0e516b204053e88276516f5d7cd9da8d1
苗景译 <miaojingyi86@gmail.com>
2024-09-07 07:24:25 +0800

    .

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.20
 
 require (
 	github.com/denisenkom/go-mssqldb v0.12.3
-	github.com/go-sql-driver/mysql v1.7.0
 	github.com/google/uuid v1.3.0
 	github.com/robertkrimen/otto v0.2.1
 	github.com/shirou/gopsutil/v3 v3.23.1
+	github.com/shopspring/decimal v1.4.0
 	github.com/sirupsen/logrus v1.9.0
 )
 
@@ -15,7 +15,6 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
--- rujs/jsvm/utils.go
+++ rujs/jsvm/utils.go
@@ -1,9 +1,8 @@
 package jsvm
 
 import (
+	"github.com/shopspring/decimal"
 	"github.com/sirupsen/logrus"
-	"strconv"
-	"strings"
 )
 
 type Utils struct {
@@ -13,60 +12,27 @@ func NewUtils() *Utils {
 	return &Utils{}
 }
 
-func (u *Utils) ToInt(value interface{}) int64 {
-	if value == nil {
-		return 0
-	}
+func (u *Utils) NewDecimal(value interface{}) decimal.Decimal {
 
 	switch value.(type) {
+	case nil:
+		return decimal.NewFromInt(0)
 	case int, int8, int16, int32, int64:
+		return decimal.NewFromInt(value.(int64))
 	case uint, uint8, uint16, uint32, uint64:
+		return decimal.NewFromUint64(value.(uint64))
 	case float32, float64:
-		return value.(int64)
+		return decimal.NewFromFloat(value.(float64))
 	case string:
-		newValue := strings.TrimSpace(value.(string))
-		if len(newValue) < 1 {
-			return 0
-		}
-
-		num, err := strconv.ParseInt(newValue, 10, 64)
+		dec, err := decimal.NewFromString(value.(string))
 		if err != nil {
-			logrus.Panicf("%q 无法转换为int64类型：%s", value, err.Error())
+			logrus.Panicf("%#v 无法转换为decimal.Decimal类型：%s", value, err.Error())
 		}
 
-		return num
+		return dec
 	default:
-		logrus.Panicf("%#v 无法转换为int64类型", value)
+		logrus.Panicf("%#v 无法转换为decimal.Decimal类型", value)
 	}
 
-	return 0
-}
-
-func (u *Utils) ToFloat(value interface{}) float64 {
-	if value == nil {
-		return 0
-	}
-
-	switch value.(type) {
-	case int, int8, int16, int32, int64:
-	case uint, uint8, uint16, uint32, uint64:
-	case float32, float64:
-		return value.(float64)
-	case string:
-		newValue := strings.TrimSpace(value.(string))
-		if len(newValue) < 1 {
-			return 0
-		}
-
-		num, err := strconv.ParseFloat(newValue, 64)
-		if err != nil {
-			logrus.Panicf("%q 无法转换为float64类型：%s", newValue, err.Error())
-		}
-
-		return num
-	default:
-		logrus.Panicf("%#v 无法转换为float64类型", value)
-	}
-
-	return 0
+	return decimal.NewFromInt(0)
 }
